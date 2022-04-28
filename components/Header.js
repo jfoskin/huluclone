19b7227e2c1d7ccd5aa34782055192263ae8d1ab
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2022-04-28 08:57:34 -0400

    titles and scrollbar added

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import Image from "next/image"
 import HeaderItem from "./HeaderItem"
 function Header() {
   return (
-    <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
         <div className="flex flex-grow justify-evenly max-w-2xl">
             <HeaderItem title='Home' Icon={HomeIcon}/>
             <HeaderItem title='Trending' Icon={LightningBoltIcon}/>
@@ -21,6 +21,7 @@ function Header() {
         </div>
         <Image className="object-contain" src='https://links.papareact.com/ua6' width={200} height={100}/>
     </header>
+
   )
 }
 
